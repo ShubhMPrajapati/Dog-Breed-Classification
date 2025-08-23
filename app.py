a179353520d84e7bc68087685a255723502e1752
Shubham Prajapati <shubhamprajapati051@gmail.com>
2025-08-23 10:50:41 -0400

    i am saying this is final

--- app.py
+++ app.py
@@ -65,4 +65,6 @@ def index():
     return render_template("index.html", prediction=prediction, img_path=img_path)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+   
